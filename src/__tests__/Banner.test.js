97d70eb1afa38a334c120dbac91c77f8abbb4c08
krishnainti <ivkrish143007@gmail.com>
2023-02-05 12:58:33 +0530

    portfolio

---
+++ src/__tests__/Banner.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import AppBanner from "../components/shared/AppBanner";
+import userEvent from "@testing-library/user-event";
+
+// This runs before each test. This is good instead of having the component render in each test case
+const setup = () => render(<AppBanner />);
+
+// Get user event
+function setupUserEvent(jsx) {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+}
+
+test("it shows the title in the banner", () => {
+  setup();
+  expect(screen.getByText(/Hi,  IAM VEKATA KRISHNA INTI/i)).toBeInTheDocument();
+});
+
+test("can download cv when clicked on download cv button", async () => {
+  const { user } = setupUserEvent(<AppBanner />);
+
+  const downloadCV = screen.getByText(/Download CV/i);
+
+  expect(downloadCV).toBeInTheDocument();
+
+  const downloadCVButton = downloadCV.parentElement.parentElement;
+
+  expect(downloadCVButton).toBeInTheDocument();
+
+  await user.click(downloadCVButton);
+});
